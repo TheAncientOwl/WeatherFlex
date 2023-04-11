fcdd489a4d281dfee63b8177a0967393eb851b7d
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-04-11 19:27:13 +0300

    code refactoring.

---
+++ Services/WeatherService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+using WeatherFlex.Model;
+
+namespace WeatherFlex.Services
+{
+    public class WeatherService
+    {
+        private static readonly string API_LINK = "https://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&hourly=temperature_2m";
+
+        readonly HttpClient httpClient;
+
+        public WeatherService()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task<Weather> FetchWeather(double latitude, double longitude)
+        {
+            Weather weather = await httpClient.GetFromJsonAsync<Weather>(string.Format(API_LINK, latitude, longitude));
+
+            return weather;
+        }
+    }
+}
---
+++ ViewModel/WeatherViewModel.cs
@@ -0,0 +1,32 @@
+﻿using WeatherFlex.Model;
+using WeatherFlex.Services;
+
+namespace WeatherFlex.ViewModels
+{
+    public class WeatherViewModel
+    {
+        Weather weather;
+        readonly WeatherService weatherService;
+        readonly GeolocationService geolocationService;
+
+        public Weather Weather { get => weather; }
+
+        public WeatherViewModel(WeatherService weatherService, GeolocationService geolocationService)
+        {
+            this.weatherService = weatherService;
+            this.geolocationService = geolocationService;
+        }
+
+        public async Task GetUserWeatherAsync()
+        {
+            Location userLocation = await geolocationService.GetLocationAsync();
+
+            await GetWeatherAsync(userLocation.Latitude, userLocation.Longitude);
+        }
+
+        public async Task GetWeatherAsync(double latitude, double longitude)
+        {
+            weather = await weatherService.FetchWeather(latitude, longitude);
+        }
+    }
+}
