e8e5d7dffa25a22029d40947cab313fb08e3f1c0
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-04-13 02:14:39 +0300

    increased forecast period

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -10,7 +10,7 @@ namespace WeatherFlex.Services
             public static readonly string Location = "&latitude={0}&longitude={1}";
             public static readonly string CurrentWeather = "&current_weather=true";
             public static readonly string Hourly = "&hourly=temperature_2m,is_day,precipitation_probability,weathercode";
-            public static readonly string ForecastDays = "&forecast_days=2";
+            public static readonly string ForecastDays = "&forecast_days=4";
         }
         private static readonly string API_LINK = "https://api.open-meteo.com/v1/forecast?" + QueryParameters.Location + QueryParameters.CurrentWeather + QueryParameters.Hourly + QueryParameters.ForecastDays;
 
