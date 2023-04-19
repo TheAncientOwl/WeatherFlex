6dc9457cefde10026c8d132b1e6dbb63f89a133e
Bita121 <francu352000@gmail.com>
2023-04-19 20:35:36 +0300

    link

--- View/AboutPage.xaml.cs
+++ View/AboutPage.xaml.cs
@@ -1,9 +1,14 @@
+using System.Windows.Input;
+
 namespace WeatherFlex.View;
 
 public partial class AboutPage : ContentPage
 {
+    public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
     public AboutPage()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
+        BindingContext = this;
     }
 }
