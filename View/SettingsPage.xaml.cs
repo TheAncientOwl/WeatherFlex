fa35bd73295bc39df9ddc99a76f1cf9007051971
alinlinca10 <71827000+alinlinca10@users.noreply.github.com>
2023-04-19 21:49:56 +0300

    settings database

--- View/SettingsPage.xaml.cs
+++ View/SettingsPage.xaml.cs
@@ -1,11 +1,19 @@
 namespace WeatherFlex.View;
-
+using WeatherFlex.Database;
 public partial class SettingsPage : ContentPage
 {
 	public SettingsPage()
 	{
 		InitializeComponent();
+
 	}
+
+    protected override async void OnAppearing()
+    {
+        var settings = new SettingsDao();
+
+        BindingContext = await settings.Get();
+    }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
