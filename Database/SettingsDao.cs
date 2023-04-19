166b3f4f2d56a2a5deea6e840cea648cd4315a15
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-04-19 21:15:22 +0300

    added settings & dao for them

---
+++ Database/SettingsDao.cs
@@ -0,0 +1,47 @@
+﻿using SQLite;
+using WeatherFlex.Model;
+
+namespace WeatherFlex.Database
+{
+    public class SettingsDao
+    {
+        SQLiteAsyncConnection connection;
+
+        public async Task InitDatabase()
+        {
+            if (connection == null)
+            {
+                string connString = Path.Combine(FileSystem.AppDataDirectory, "weather_flex.db");
+
+                connection = new SQLiteAsyncConnection(connString);
+
+                await connection.CreateTableAsync<Settings>();
+            }
+        }
+
+        public async Task<int> Insert(Settings settings)
+        {
+            await InitDatabase();
+
+            return await connection.InsertAsync(settings);
+        }
+
+        public async Task<Settings> Get()
+        {
+            await InitDatabase();
+
+            List<Settings> list = await connection.QueryAsync<Settings>("SELECT * FROM settings");
+
+            return list[0];
+        }
+
+        public async Task<int> Update(Settings settings)
+        {
+            await InitDatabase();
+
+            await connection.ExecuteAsync("DELETE FROM settings WHERE 1 = 1");
+
+            return await Insert(settings);
+        }
+    }
+}
---
+++ Model/Settings.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+
+namespace WeatherFlex.Model
+{
+    [Table("settings")]
+    public class Settings
+    {
+        [Column("id")]
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+
+
+        [Column("preffers_celsius")]
+        public bool PreffersCelsius { get; set; }
+
+    }
+}
