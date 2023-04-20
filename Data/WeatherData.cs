103bdd2aa5f55b10c25bb7698faed0edbb9273c0
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-04-20 19:44:54 +0300

    added data persistance during app lifetime (weather data). TODO: refetch data?

---
+++ Data/WeatherData.cs
@@ -0,0 +1,32 @@
+﻿using WeatherFlex.Model.Weather;
+using WeatherFlex.Services;
+
+namespace WeatherFlex.Data
+{
+    public class WeatherData
+    {
+        readonly Dictionary<Tuple<double, double>, WeatherAPI> data = new();
+
+        static WeatherData instance;
+
+        public static async Task<WeatherAPI> GetFor(double latitude, double longitude)
+        {
+            instance ??= new WeatherData();
+
+            Tuple<double, double> location = new(latitude, longitude);
+            if (instance.data.ContainsKey(location))
+            {
+                return instance.data[location];
+            }
+
+            WeatherAPI weatherApi = await new WeatherService().FetchWeather(latitude, longitude);
+            weatherApi.LocationProperties = await new GeolocationService().GetLocationPropertiesAsync(latitude, longitude);
+
+            instance.data.Add(location, weatherApi);
+
+            return weatherApi;
+        }
+
+        private WeatherData() { }
+    }
+}
